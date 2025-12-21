e97e808c95bb67a9b3bed7411d94b00754a6f932
peterpardo <peterpardo123@gmail.com>
2025-12-21 23:21:30 +0800

    chore: setup eslint rulest to error specific file import patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,18 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["../*"],
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
