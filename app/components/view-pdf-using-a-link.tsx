5d33d4162df34d617091afbf03adac1f4092b868
peterpardo <peterpardo123@gmail.com>
2025-12-21 23:37:53 +0800

    feat: add implementation for viewing pdf using a link

---
+++ app/components/view-pdf-using-a-link.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ViewPDFUsingALink() {
+  return (
+    <a href="/sample.pdf" target="_blank" rel="noopener noreferrer">
+      View PDF
+    </a>
+  );
+}
